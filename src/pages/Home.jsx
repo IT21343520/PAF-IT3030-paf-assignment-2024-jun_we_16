a0877a0cb207c140f8134ad852d852f8b9e7a4cc
Rovinya Wijerama <it21343520@my.sliit.lk>
2024-05-09 20:09:24 +0530

    home.js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ import { AiFillEdit } from "react-icons/ai";
 import TimeAgo from "../components/TimeAgo";
 import back1 from "../assets/back1.jpg";
 
-//
+//home
 const Home = () => {
   const [post, setPost] = useState([]);
 
